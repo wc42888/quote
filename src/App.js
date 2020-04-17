1a670f8027f39afb8568424e8b41b4e708e9a4f5
chao wang <wc42888@hotmail.com>
2020-04-18 09:26:13 +1000

    added Title to the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Title } from './components';
+import { LIGHT_BLUE } from './typography';
 
 function App() {
-  return <div>quote</div>;
+  return (
+    <Container>
+      <Title title="Quick Quote" />
+      <Line />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  height: 100vh;
+  flex-direction: column;
+  margin: 10px;
+`;
+
+const Line = styled.hr`
+  margin: 0;
+  border-top: 1pt solid ${LIGHT_BLUE};
+`;
 
 export default App;
