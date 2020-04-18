38be62812dc76fe47c9aeafff1167f4e2a22b22c
chao wang <wc42888@hotmail.com>
2020-04-18 19:40:16 +1000

    added new Error component

---
+++ src/components/Error.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { NORMAL_SIZE, RED } from '../typography';
+
+export default styled.div`
+  font-size: ${NORMAL_SIZE};
+  color: ${RED};
+  min-height: ${NORMAL_SIZE};
+`;
