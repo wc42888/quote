84e5f5a9ab71214218e52ae7139a2218c60b3c2e
chao wang <wc42888@hotmail.com>
2020-04-18 15:50:48 +1000

    added a new button component

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { BLUE, NORMAL_SIZE } from '../typography';
+
+export default styled.button.attrs(({ isSubmitting }) => ({
+  disabled: isSubmitting,
+}))`
+  background-color: ${({ isSubmitting }) =>
+    isSubmitting ? '#cccccc' : `${BLUE}`};
+  height: 80px;
+  min-height: 40px;
+  width: 250px;
+  border-radius: 40px;
+  font-size: ${NORMAL_SIZE};
+  color: white;
+  outline: none;
+`;
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Field } from './Field';
 export { default as MobileField } from './MobileField';
 export { default as SelectField } from './SelectField';
 export { default as FieldContainer } from './FieldContainer';
+export { default as Button } from './Button';
