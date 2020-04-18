ba250f54ca20dc5594b3bc4053b9d1bba710ed7f
chao wang <wc42888@hotmail.com>
2020-04-18 12:04:32 +1000

    added currencies constants

---
+++ src/constants/currencies.js
@@ -0,0 +1,474 @@
+export default [
+  {
+    value: 'USD',
+    option: 'US Dollar',
+  },
+  {
+    value: 'CAD',
+    option: 'Canadian Dollar',
+  },
+  {
+    value: 'EUR',
+    option: 'Euro',
+  },
+  {
+    value: 'AED',
+    option: 'United Arab Emirates Dirham',
+  },
+  {
+    value: 'AFN',
+    option: 'Afghan Afghani',
+  },
+  {
+    value: 'ALL',
+    option: 'Albanian Lek',
+  },
+  {
+    value: 'AMD',
+    option: 'Armenian Dram',
+  },
+  {
+    value: 'ARS',
+    option: 'Argentine Peso',
+  },
+  {
+    value: 'AUD',
+    option: 'Australian Dollar',
+  },
+  {
+    value: 'AZN',
+    option: 'Azerbaijani Manat',
+  },
+  {
+    value: 'BAM',
+    option: 'Bosnia-Herzegovina Convertible Mark',
+  },
+  {
+    value: 'BDT',
+    option: 'Bangladeshi Taka',
+  },
+  {
+    value: 'BGN',
+    option: 'Bulgarian Lev',
+  },
+  {
+    value: 'BHD',
+    option: 'Bahraini Dinar',
+  },
+  {
+    value: 'BIF',
+    option: 'Burundian Franc',
+  },
+  {
+    value: 'BND',
+    option: 'Brunei Dollar',
+  },
+  {
+    value: 'BOB',
+    option: 'Bolivian Boliviano',
+  },
+  {
+    value: 'BRL',
+    option: 'Brazilian Real',
+  },
+  {
+    value: 'BWP',
+    option: 'Botswanan Pula',
+  },
+  {
+    value: 'BYR',
+    option: 'Belarusian Ruble',
+  },
+  {
+    value: 'BZD',
+    option: 'Belize Dollar',
+  },
+  {
+    value: 'CDF',
+    option: 'Congolese Franc',
+  },
+  {
+    value: 'CHF',
+    option: 'Swiss Franc',
+  },
+  {
+    value: 'CLP',
+    option: 'Chilean Peso',
+  },
+  {
+    value: 'CNY',
+    option: 'Chinese Yuan',
+  },
+  {
+    value: 'COP',
+    option: 'Colombian Peso',
+  },
+  {
+    value: 'CRC',
+    option: 'Costa Rican Colón',
+  },
+  {
+    value: 'CVE',
+    option: 'Cape Verdean Escudo',
+  },
+  {
+    value: 'CZK',
+    option: 'Czech Republic Koruna',
+  },
+  {
+    value: 'DJF',
+    option: 'Djiboutian Franc',
+  },
+  {
+    value: 'DKK',
+    option: 'Danish Krone',
+  },
+  {
+    value: 'DOP',
+    option: 'Dominican Peso',
+  },
+  {
+    value: 'DZD',
+    option: 'Algerian Dinar',
+  },
+  {
+    value: 'EEK',
+    option: 'Estonian Kroon',
+  },
+  {
+    value: 'EGP',
+    option: 'Egyptian Pound',
+  },
+  {
+    value: 'ERN',
+    option: 'Eritrean Nakfa',
+  },
+  {
+    value: 'ETB',
+    option: 'Ethiopian Birr',
+  },
+  {
+    value: 'GBP',
+    option: 'British Pound Sterling',
+  },
+  {
+    value: 'GEL',
+    option: 'Georgian Lari',
+  },
+  {
+    value: 'GHS',
+    option: 'Ghanaian Cedi',
+  },
+  {
+    value: 'GNF',
+    option: 'Guinean Franc',
+  },
+  {
+    value: 'GTQ',
+    option: 'Guatemalan Quetzal',
+  },
+  {
+    value: 'HKD',
+    option: 'Hong Kong Dollar',
+  },
+  {
+    value: 'HNL',
+    option: 'Honduran Lempira',
+  },
+  {
+    value: 'HRK',
+    option: 'Croatian Kuna',
+  },
+  {
+    value: 'HUF',
+    option: 'Hungarian Forint',
+  },
+  {
+    value: 'IDR',
+    option: 'Indonesian Rupiah',
+  },
+  {
+    value: 'ILS',
+    option: 'Israeli New Sheqel',
+  },
+  {
+    value: 'INR',
+    option: 'Indian Rupee',
+  },
+  {
+    value: 'IQD',
+    option: 'Iraqi Dinar',
+  },
+  {
+    value: 'IRR',
+    option: 'Iranian Rial',
+  },
+  {
+    value: 'ISK',
+    option: 'Icelandic Króna',
+  },
+  {
+    value: 'JMD',
+    option: 'Jamaican Dollar',
+  },
+  {
+    value: 'JOD',
+    option: 'Jordanian Dinar',
+  },
+  {
+    value: 'JPY',
+    option: 'Japanese Yen',
+  },
+  {
+    value: 'KES',
+    option: 'Kenyan Shilling',
+  },
+  {
+    value: 'KHR',
+    option: 'Cambodian Riel',
+  },
+  {
+    value: 'KMF',
+    option: 'Comorian Franc',
+  },
+  {
+    value: 'KRW',
+    option: 'South Korean Won',
+  },
+  {
+    value: 'KWD',
+    option: 'Kuwaiti Dinar',
+  },
+  {
+    value: 'KZT',
+    option: 'Kazakhstani Tenge',
+  },
+  {
+    value: 'LBP',
+    option: 'Lebanese Pound',
+  },
+  {
+    value: 'LKR',
+    option: 'Sri Lankan Rupee',
+  },
+  {
+    value: 'LTL',
+    option: 'Lithuanian Litas',
+  },
+  {
+    value: 'LVL',
+    option: 'Latvian Lats',
+  },
+  {
+    value: 'LYD',
+    option: 'Libyan Dinar',
+  },
+  {
+    value: 'MAD',
+    option: 'Moroccan Dirham',
+  },
+  {
+    value: 'MDL',
+    option: 'Moldovan Leu',
+  },
+  {
+    value: 'MGA',
+    option: 'Malagasy Ariary',
+  },
+  {
+    value: 'MKD',
+    option: 'Macedonian Denar',
+  },
+  {
+    value: 'MMK',
+    option: 'Myanma Kyat',
+  },
+  {
+    value: 'MOP',
+    option: 'Macanese Pataca',
+  },
+  {
+    value: 'MUR',
+    option: 'Mauritian Rupee',
+  },
+  {
+    value: 'MXN',
+    option: 'Mexican Peso',
+  },
+  {
+    value: 'MYR',
+    option: 'Malaysian Ringgit',
+  },
+  {
+    value: 'MZN',
+    option: 'Mozambican Metical',
+  },
+  {
+    value: 'NAD',
+    option: 'Namibian Dollar',
+  },
+  {
+    value: 'NGN',
+    option: 'Nigerian Naira',
+  },
+  {
+    value: 'NIO',
+    option: 'Nicaraguan Córdoba',
+  },
+  {
+    value: 'NOK',
+    option: 'Norwegian Krone',
+  },
+  {
+    value: 'NPR',
+    option: 'Nepalese Rupee',
+  },
+  {
+    value: 'NZD',
+    option: 'New Zealand Dollar',
+  },
+  {
+    value: 'OMR',
+    option: 'Omani Rial',
+  },
+  {
+    value: 'PAB',
+    option: 'Panamanian Balboa',
+  },
+  {
+    value: 'PEN',
+    option: 'Peruvian Nuevo Sol',
+  },
+  {
+    value: 'PHP',
+    option: 'Philippine Peso',
+  },
+  {
+    value: 'PKR',
+    option: 'Pakistani Rupee',
+  },
+  {
+    value: 'PLN',
+    option: 'Polish Zloty',
+  },
+  {
+    value: 'PYG',
+    option: 'Paraguayan Guarani',
+  },
+  {
+    value: 'QAR',
+    option: 'Qatari Rial',
+  },
+  {
+    value: 'RON',
+    option: 'Romanian Leu',
+  },
+  {
+    value: 'RSD',
+    option: 'Serbian Dinar',
+  },
+  {
+    value: 'RUB',
+    option: 'Russian Ruble',
+  },
+  {
+    value: 'RWF',
+    option: 'Rwandan Franc',
+  },
+  {
+    value: 'SAR',
+    option: 'Saudi Riyal',
+  },
+  {
+    value: 'SDG',
+    option: 'Sudanese Pound',
+  },
+  {
+    value: 'SEK',
+    option: 'Swedish Krona',
+  },
+  {
+    value: 'SGD',
+    option: 'Singapore Dollar',
+  },
+  {
+    value: 'SOS',
+    option: 'Somali Shilling',
+  },
+  {
+    value: 'SYP',
+    option: 'Syrian Pound',
+  },
+  {
+    value: 'THB',
+    option: 'Thai Baht',
+  },
+  {
+    value: 'TND',
+    option: 'Tunisian Dinar',
+  },
+  {
+    value: 'TOP',
+    option: 'Tongan Paʻanga',
+  },
+  {
+    value: 'TRY',
+    option: 'Turkish Lira',
+  },
+  {
+    value: 'TTD',
+    option: 'Trinidad and Tobago Dollar',
+  },
+  {
+    value: 'TWD',
+    option: 'New Taiwan Dollar',
+  },
+  {
+    value: 'TZS',
+    option: 'Tanzanian Shilling',
+  },
+  {
+    value: 'UAH',
+    option: 'Ukrainian Hryvnia',
+  },
+  {
+    value: 'UGX',
+    option: 'Ugandan Shilling',
+  },
+  {
+    value: 'UYU',
+    option: 'Uruguayan Peso',
+  },
+  {
+    value: 'UZS',
+    option: 'Uzbekistan Som',
+  },
+  {
+    value: 'VEF',
+    option: 'Venezuelan Bolívar',
+  },
+  {
+    value: 'VND',
+    option: 'Vietnamese Dong',
+  },
+  {
+    value: 'XAF',
+    option: 'CFA Franc BEAC',
+  },
+  {
+    value: 'XOF',
+    option: 'CFA Franc BCEAO',
+  },
+  {
+    value: 'YER',
+    option: 'Yemeni Rial',
+  },
+  {
+    value: 'ZAR',
+    option: 'South African Rand',
+  },
+  {
+    value: 'ZMK',
+    option: 'Zambian Kwacha',
+  },
+];
