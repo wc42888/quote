f94981bcfe4806d0eb00ebcb32c3d04a6ddbebaa
chao wang <wc42888@hotmail.com>
2020-04-18 10:49:02 +1000

    added a new typography constant

--- src/typography/fontSize.js
+++ src/typography/fontSize.js
@@ -1,1 +1,2 @@
 export const LARGE_SIZE = '32pt';
+export const NORMAL_SIZE = '12pt';
--- src/typography/index.js
+++ src/typography/index.js
@@ -1,4 +1,4 @@
-import { LARGE_SIZE } from './fontSize';
+import { LARGE_SIZE, NORMAL_SIZE } from './fontSize';
 import { LIGHT_BLUE, LIGHT_GREY, RED } from './color';
 
-export { LARGE_SIZE, LIGHT_BLUE, LIGHT_GREY, RED };
+export { LARGE_SIZE, LIGHT_BLUE, LIGHT_GREY, RED, NORMAL_SIZE };
